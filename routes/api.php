6f9adeb81d50cb47242e577eab7cb9d644da9505
Archie Jefferson Em <archie.em99@gmail.com>
2024-06-16 13:25:27 +0800

    fix: sign in csrf mismatch

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,6 @@ use App\Http\Controllers\API\v1\Authentication\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -29,11 +18,17 @@ Route::prefix('v1/')->name('api.v1.')->group(function() {
     });
 
     //AUTHENTICATION
-    Route::prefix('auth')->name('auth.')->group(function() {
+    Route::middleware('web')->prefix('auth')->name('auth.')->group(function() {
         Route::controller(AuthController::class)->group(function() {
             Route::post('signup', 'signup')->name('signup');
             Route::post('signin', 'signin')->name('signin');
         });
     });
-
+    
+    Route::get('/csrf-token', function () {
+        return response()->json(['csrf_token' => csrf_token()]);
+    })->middleware('web');
 });
+
+
+
