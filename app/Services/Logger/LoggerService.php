510ccc49afd78667f93f9f937cdf6bb1f4ba9906
Archie Jefferson Em <archie.em99@gmail.com>
2024-06-16 12:07:41 +0800

    chore: docblock

--- app/Services/Logger/LoggerService.php
+++ app/Services/Logger/LoggerService.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Log;
 class LoggerService 
 {
     /**
-     * @param $error
+     * @param \Throwable $error
      * @return void
      */
     public function logError($error)
