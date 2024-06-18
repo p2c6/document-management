de2b8b27b44831c391bee7d76dbd19bab8cc1897
Archie Jefferson Em <archie.em99@gmail.com>
2024-06-18 16:58:40 +0800

    chore: update docblock

--- app/Services/Response/ResponseService.php
+++ app/Services/Response/ResponseService.php
@@ -2,8 +2,6 @@
 
 namespace App\Services\Response;
 
-use App\Models\Roles;
-use App\Models\User;
 use App\Services\Logger\LoggerService;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -13,23 +11,24 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class ResponseService 
 {
     /**
-     * @param string $request
-     * @param string $message
-     * @param object $data
-     * @param JsonResponse $response
+     * Handles the response for the given request.
+     *
+     * @param string $status The status of the response.
+     * @param string|null $message The message for the response.
+     * @param mixed $data The data to include in the response.
+     * @param int $response The HTTP status code for the response.
      * 
-     * @return object 
+     * @return JsonResponse 
      */
-    public function response($status, $message = null, $data = null, $response) : object
+    public function response(string $status, ?string $message, $data, int $response): JsonResponse
     {
         if ($status === "error")  {
-            
             if (!is_null($data)) {
                 $service = new LoggerService();
                 $service->logError($data);
             }
 
-            return response()->json(["status" => $status, "message" => $message, ], $response);
+            return response()->json(["status" => $status, "message" => $message], $response);
         }
 
         return response()->json(["status" => $status, "message" => $message, "data" => $data], $response);
