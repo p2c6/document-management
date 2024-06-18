195a6106f4ec43cb6b24b1bc765c8ba785a9e0be
Archie Jefferson Em <archie.em99@gmail.com>
2024-06-18 22:49:58 +0800

    refactor: web middleware

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1/')->name('api.v1.')->group(function() {
+Route::middleware(['web'])->prefix('v1/')->name('api.v1.')->group(function() {
     //ROLES
     Route::prefix('roles')->name('roles.')->group(function() {
         Route::controller(RoleController::class)->group(function() {
@@ -18,7 +18,7 @@ Route::prefix('v1/')->name('api.v1.')->group(function() {
     });
 
     //AUTHENTICATION
-    Route::middleware('web')->prefix('auth')->name('auth.')->group(function() {
+    Route::prefix('auth')->name('auth.')->group(function() {
         Route::controller(AuthController::class)->group(function() {
             Route::post('signup', 'signup')->name('signup');
             Route::post('signin', 'signin')->name('signin');
@@ -28,7 +28,7 @@ Route::prefix('v1/')->name('api.v1.')->group(function() {
     //CSRF
     Route::get('/csrf-token', function () {
         return response()->json(['csrf_token' => csrf_token()]);
-    })->middleware('web');
+    });
 });
 
 
