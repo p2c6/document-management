12ce67740bba86db552702902bf8a54ee03956f0
Archie Jefferson Em <archie.em99@gmail.com>
2024-06-22 21:37:35 +0800

    chore: add docblock

--- app/Http/Controllers/API/v1/Authentication/AuthController.php
+++ app/Http/Controllers/API/v1/Authentication/AuthController.php
@@ -8,18 +8,40 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    
+    /**
+     * The email authentication service instance.
+     *
+     * @var EmailAuthService
+     */
     public $service;
 
+    /**
+     * Class constructor.
+     *
+     * @param EmailAuthService $service The email authentication service instance.
+     */
     public function __construct(EmailAuthService $service)
     {
         $this->service = $service;
     }
+    
+    /**
+     * Handle user signup request.
+     *
+     * @param \Illuminate\Http\Request $request The HTTP request object containing signup data.
+     * @return \Illuminate\Http\Response
+     */
     public function signup(Request $request)
     {
         return $this->service->signup($request);
     }
 
+    /**
+     * Handle user sign-in request.
+     *
+     * @param \Illuminate\Http\Request $request The HTTP request object containing sign-in data.
+     * @return \Illuminate\Http\Response
+     */
     public function signin(Request $request)
     {
         return $this->service->signin($request);
