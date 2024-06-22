24cfb7398be612767cc4fdfb9439826a1d46fd76
Archie Jefferson Em <archie.em99@gmail.com>
2024-06-22 21:42:47 +0800

    feat: add application model and migration

---
+++ app/Models/Application.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Application extends Model
+{
+    use HasFactory;
+
+    /**
+     * Get all the documents for the application.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function documents(): HasMany
+    {
+        return $this->hasMany(Document::class, 'application_id', 'id');
+    }
+
+    /**
+     * Get the user that owns the application.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the for coding that onws the application.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function forCoding(): HasOne
+    {
+        return $this->hasOne(ForCoding::class, 'application_id', 'id');
+    }
+}
