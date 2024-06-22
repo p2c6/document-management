9275e314341398f0ea618daf82caf8b64b2b0d31
Archie Jefferson Em <archie.em99@gmail.com>
2024-06-22 21:38:38 +0800

    feat: update model relationshio

--- app/Models/ForCoding.php
+++ app/Models/ForCoding.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ForCoding extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the application that owns the for coding.
+     * 
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+     * 
+     */
+    public function application(): BelongsTo
+    {
+        return $this->belongsTo(Application::class);
+    }
 }
