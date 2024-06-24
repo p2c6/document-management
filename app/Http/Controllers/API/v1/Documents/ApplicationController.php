5cc1814313839f16a94a6a676416a8a8d8b50e6c
Archie Jefferson Em <archie.em99@gmail.com>
2024-06-24 23:54:04 +0800

    feat: implement application list; implement application show

--- app/Http/Controllers/API/v1/Documents/ApplicationController.php
+++ app/Http/Controllers/API/v1/Documents/ApplicationController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\API\v1\Documents;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Application\ApplicationCollection;
+use App\Http\Resources\Application\ApplicationResource;
 use App\Models\Application;
 use App\Services\Application\ApplicationService;
 use Illuminate\Http\Request;
@@ -28,6 +30,28 @@ class ApplicationController extends Controller
     }
 
     /**
+     * List of all applications.
+     * 
+     * @return \App\Http\Resources\ApplicationResource
+     */
+    public function index()
+    {
+        return new ApplicationCollection(Application::paginate());
+    }
+
+    /**
+     * Show specific application.
+     * 
+     * @param int $id The id of the application.
+     * 
+     * @return \App\Http\Resources\ApplicationResource
+     */
+    public function show(int $id)
+    {
+        return new ApplicationResource(Application::findOrFail($id));
+    }
+
+    /**
      * Store a new application.
      *
      * @param \Illuminate\Http\Request $request The HTTP request object containing application data.
@@ -37,7 +61,6 @@ class ApplicationController extends Controller
     {
         return $this->service->store($request);
     }
-
 
     public function update(Application $application, Request $request)
     {
