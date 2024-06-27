ab5b1db50b92127c3e7f05d5919b35c3c5de97a4
Archie Jefferson Em <archie.em99@gmail.com>
2024-06-27 16:04:44 +0800

    refactor: temporary file implementation

--- app/Http/Controllers/TemporaryFileController.php
+++ app/Http/Controllers/TemporaryFileController.php
@@ -2,77 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\TemporaryFile;
+use App\Services\TemporaryUpload\TemporaryUploadService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class TemporaryFileController extends Controller
 {
-    public function upload(Request $request)
+    /**
+     * The temporary upload service instance.
+     * 
+     * @var TemporaryUploadService
+     */
+    private $service;
+
+    /**
+     * TemporaryFileController constructor.
+     * 
+     * @param TemporaryService $service The instance of TemporaryUploadService.
+     */
+    public function __construct(TemporaryUploadService $service)
     {
-        if ($request->hasFile('filepond.*')) {
-            $folders = [];
-            $files = $request->file('filepond');
-            foreach ($files as $file) {
-                $original = $file->getClientOriginalName();
-                $extension = $file->extension();
-
-                $filename = \uniqid() . '-' . now()->timestamp . '.' . $extension;
-                $folder = \uniqid() . '-' . now()->timestamp;
-
-                $folders[] = $folder;
-
-                $file = Storage::putFileAs('uploads/tmp/' . $folder, $file, $filename);
-
-                if (!$file) // Will not create if storeAs fails
-                    return response()->json(['status' => 'failed', 'message' => "Something's wrong in Storing File(s)"]);
-
-                TemporaryFile::create([
-                    'folder' => $folder,
-                    'filename' => $filename,
-                    'file' => $file,
-                    'original' => $original,
-                ]);
-            }
-            return $folders;
-        } 
-        
-        if ($request->hasFile('filepond')) {
-            $file = $request->file('filepond');
-            $original = $file->getClientOriginalName();
-            $extension = $file->extension();
-
-            $filename = \uniqid() . '-' . now()->timestamp . '.' . $extension;
-            $folder = \uniqid() . '-' . now()->timestamp;
-
-
-            $file = Storage::putFileAs('uploads/tmp/' . $folder,  $file, $filename);
-
-            if (!$file) // Will not create if storeAs fails
-                return response()->json(['status' => 'failed', 'message' => "Something's wrong in Storing File(s)"]);
-
-            TemporaryFile::create([
-                'folder' => $folder,
-                'filename' => $filename,
-                'file' => $file,
-                'original' => $original,
-            ]);
-
-            return $folder;
-        }
-        return '';
+        $this->service = $service;
     }
 
-    public function revert(Request $request)
+    /**
+     * Handle temporary file upload.
+     * 
+     * @param \Illuminate\Http\Request $request The HTTP request object containing upload data.
+     * @return mixed|string
+     */
+    public function upload(Request $request)
     {
-        info($request);
-        $temp_file = $request->filepond; // Directory
+        return $this->service->upload($request);
+    }
 
-
-        if ($temp_file) {
-            TemporaryFile::where('folder', $temp_file)->delete();
-            Storage::deleteDirectory('uploads/tmp/' . $temp_file);
-            return response('');
-        }
+    /**
+     * Handle temporary file revert.
+     * 
+     * @param \Illuminate\Http\Request $request The HTTP request object containing upload data.
+     * @return mixed|string
+     */
+    public function revert(Request $request): mixed
+    {
+        return $this->service->revert($request);
     }
 }
