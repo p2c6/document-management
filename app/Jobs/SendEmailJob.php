4823310881b3ee4376f4d99a60299a400e0824a5
Archie Jefferson Em <archie.em99@gmail.com>
2024-06-29 20:30:08 +0800

    add send email job when account created

--- app/Jobs/SendEmailJob.php
+++ app/Jobs/SendEmailJob.php
@@ -2,24 +2,26 @@
 
 namespace App\Jobs;
 
+use App\Mail\AccountCreated;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
 
 class SendEmailJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public $tries = 3;
-
+    public $data;
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct($data)
     {
-        //
+        $this->data = $data;
     }
 
     /**
@@ -27,8 +29,7 @@ class SendEmailJob implements ShouldQueue
      */
     public function handle(): void
     {
-        // sleep(5);
-        throw new \Exception('Failed!');
+        Mail::send(new AccountCreated($this->data));
     }
 
     public function failed(\Throwable $e): void
